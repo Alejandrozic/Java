8a610fdce543683678eaf5d7bcb7d2d2f2891ee8
AZ <alejandrozic@gmail.com>
2018-12-01 10:38:06 -0500

    init add

---
+++ src/Common.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Common
+{
+    public static void main(String[] args)
+    {
+        // Scanner
+        String s;
+        int n;
+        Scanner scanner;
+
+        scanner = new Scanner(System.in);
+        s = scanner.nextLine();
+        n = scanner.nextInt();
+
+
+    }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass
+{
+    // Method Overloading
+    public void Method() { }
+    public void Method(String s){ }
+    public void Method(String s, int n){ }
+
+}
