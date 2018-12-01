0af82750f2de73ee2374f4ed5da9b5d9095982d5
Alejandrozic <alejandrozic@gmail.com>
2018-12-01 16:20:36 +0000

    first commit

--- src/Searches.java
+++ src/Searches.java
@@ -1,2 +1,46 @@
 public class Searches {
+
+    public void linearSearch(){
+        /**
+         * Linear Search
+         *  - searches in linear fashion
+         *  - O(N) Time Complexity
+         * */
+
+        int[] arr = {1,2,3};
+        int find = 2;
+
+        for (int i: arr) {
+            if (i == find){
+                //...
+            }
+        }
+    }
+
+    public boolean binarySearch(){
+        /**
+         * Binary Search on sorted array
+         *  - searches at mid points of data,
+         *    re-sizes search if not found to half (left or right)
+         *  - O(logN) Time Complexity
+         * */
+
+        int[] arr = {1,5,11,21,32,45,67,89,109};
+        int find = 1;
+        int low = 0;
+        int mid;
+        int high = arr.length;
+
+        while (true){
+            mid = (low + high) / 2;
+            if ( find == arr[mid])
+                return true;
+            else if (find < arr[mid])
+                high = mid;
+            else
+                low = mid;
+            if (low >= high - 1 )
+                return false;
+        }
+    }
 }
--- src/Sorting.java
+++ src/Sorting.java
@@ -1,2 +1,57 @@
+import java.util.Arrays;
+
 public class Sorting {
+
+    public static void main(String[] args) {
+        selectionSort();
+    }
+
+    public static void bubbleSort(){
+        /**
+         * Bubble Sort
+         *  - start at first element, compare to next, and swap if next element is smaller
+         *    do this until the end
+         *    then start at first element + length of array, until completed
+         *  - O(N*logN) Time Complexity
+         * */
+
+        int[] arr = {5,32,8,4,65,9,1,3};
+        int tmp;
+
+        for ( int i = 0; i < arr.length-1; i ++){
+            for ( int j = i+1; j < arr.length; j++){
+                if (arr[i] > arr[j]) {
+                    tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(){
+        /**
+         * Selection Sort
+         *  - finds smallest and swaps it with the last element
+         * */
+
+        int[] arr = {11,9,17,5,12};
+        int tmp;
+        int minIndex;
+
+        for (int i = 0; i < arr.length - 1 ; i++){
+            minIndex = i;
+            for (int j = i+1; j < arr.length; j++){
+                if ( arr[i] > arr[j] ){
+                    if ( arr[j] < arr[minIndex] )
+                        minIndex = j;
+                }
+            }
+            if ( i != minIndex ){
+                tmp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
 }
