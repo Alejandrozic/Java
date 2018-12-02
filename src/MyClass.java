f9b6e36f9b7724a5a311b91b83b670e2c3e08fff
Alejandrozic <alejandrozic@gmail.com>
2018-12-02 16:13:22 +0000

    minor updates

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,8 +1,13 @@
-public class MyClass
-{
+public class MyClass extends MyAbstractClass implements MyInterface {
     // Method Overloading
     public void Method() { }
     public void Method(String s){ }
     public void Method(String s, int n){ }
+
+    // Override parent Object's default toString method
+    @Override
+    public String toString(){
+        return "";
+    }
 
 }
